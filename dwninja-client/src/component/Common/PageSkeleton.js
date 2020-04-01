441e81eeb2e41037e990e2bf1b2230646050e7c8
Evgeny Kabardinsky <ekabardinsky@gmail.com>
2020-04-01 20:33:09 +0500

    Remove unused operation

--- dwninja-client/src/component/Common/PageSkeleton.js
+++ dwninja-client/src/component/Common/PageSkeleton.js
@@ -17,7 +17,6 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import VariableBar from "../Variables/VariableBar";
 
 class PageSkeleton extends Component {
 
