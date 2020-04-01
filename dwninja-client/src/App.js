be93d635f06a65e5b75a8e6e8a0e0ea145f1789f
Evgeny Kabardinsky <ekabardinsky@gmail.com>
2020-04-01 21:13:48 +0500

    Add draft theme

--- dwninja-client/src/App.js
+++ dwninja-client/src/App.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
+import { ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import {red} from "@material-ui/core/colors";
 
 export default class App extends Component {
     render() {
+        const theme = createMuiTheme({
+            palette: {
+                primary: {
+                    main: '#686962'
+                },
+                background: {
+                    paper: '#a7a5a0'
+                }
+            }
+        });
+
         return (
-            <Switch>
-                <Route exact path='/' render={() => <Main/>}/>
-                <Route exact path='/login' render={() => <Login/>}/>
-            </Switch>
+            <ThemeProvider theme={theme}>
+                <Switch>
+                    <Route exact path='/' render={() => <Main/>}/>
+                    <Route exact path='/login' render={() => <Login/>}/>
+                </Switch>
+            </ThemeProvider>
         );
     }
 }
