a10273aa19fe7702dea29095c58b459a9a41a1a5
Evgeny Kabardinsky <ekabardinsky@gmail.com>
2020-04-13 19:26:54 +0500

    Say thanks pop up

--- dwninja-client/src/component/Common/MainMenu.js
+++ dwninja-client/src/component/Common/MainMenu.js
@@ -5,13 +5,18 @@ import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
 import CollectionsEditor from "./CollectionsEditor";
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
 import Dialog from '@material-ui/core/Dialog';
 import {changeTheme, saveCollection} from "../../redux/actions";
 import Drawer from '@material-ui/core/Drawer';
 import {post} from "../../utils/Api";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import GitHubButton from 'react-github-btn';
+import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
 
 const themes = [
     "clouds",
@@ -62,7 +67,8 @@ class MainMenu extends Component {
             themesAnchorEl: null,
             openCollectionEditor: false,
             openThemes: false,
-            savingState: false
+            savingState: false,
+            openSayThanks: false
         };
     }
 
@@ -103,6 +109,10 @@ class MainMenu extends Component {
         })
     }
 
+    closeOrOpenSayThanks(event) {
+        this.setState({openSayThanks: !this.state.openSayThanks, open: false});
+    }
+
     render() {
         const access_token = window.localStorage.getItem('access_token');
         let isLoginPage = window.location.href.includes("/login");
@@ -125,12 +135,62 @@ class MainMenu extends Component {
                 <MenuItem onClick={this.closeOrOpenThemes.bind(this)}>
                     Themes
                 </MenuItem>
+                <MenuItem onClick={this.closeOrOpenSayThanks.bind(this)}>
+                    Say thanks
+                </MenuItem>
                 {authorized && <MenuItem onClick={this.logout.bind(this)}>Log out</MenuItem>}
                 {!authorized && !isLoginPage && <MenuItem onClick={this.logout.bind(this)}>Log in</MenuItem>}
             </Menu>
             <Dialog onClose={this.closeOrOpenCollectionsEditor.bind(this)} open={this.state.openCollectionEditor}>
                 <DialogTitle>Collections Editor</DialogTitle>
                 <CollectionsEditor/>
+            </Dialog>
+            <Dialog onClose={this.closeOrOpenSayThanks.bind(this)} open={this.state.openSayThanks}>
+                <DialogTitle>Say thanks</DialogTitle>
+                <DialogContent>
+                    <Grid container spacing={2} justify={"flex-start"} alignItems={"center"}>
+                        <Grid item xs={12}>
+                            <Typography>DW Ninja is completely free to use, the code is open source, and I provide
+                                hosting for
+                                this application.
+                                Donations from users help pay bills and allow me dedicate time to the continued
+                                development and
+                                support of this project.
+                                However, money is not the only way to support the project. If you‘ve found DW Ninja
+                                useful, feel
+                                free to contribute it.</Typography>
+                        </Grid>
+
+                        <Grid item xs={3}>
+                            <GitHubButton
+                                aria-label={"Star ekabardinsky/dwninja-app on GitHub"}
+                                title={"Star ekabardinsky/dwninja-app on GitHub"}
+                                data-size="large"
+                                data-icon="octicon-star"
+                                data-show-count="true"
+                                href="https://github.com/ekabardinsky/dwninja-app">Star</GitHubButton>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <GitHubButton
+                                aria-label={"Issue ekabardinsky/dwninja-app on GitHub"}
+                                title={"Issue ekabardinsky/dwninja-app on GitHub"}
+                                data-size="large"
+                                data-icon="octicon-issue-opened"
+                                data-show-count="true"
+                                href="https://github.com/ekabardinsky/dwninja-app/issues">Issue</GitHubButton>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <GitHubButton
+                                aria-label={"Follow @ekabardinsky on GitHub"}
+                                title={"Follow @ekabardinsky on GitHub"}
+                                data-size="large"
+                                data-show-count="true"
+                                href="https://github.com/ekabardinsky">Follow</GitHubButton>
+                        </Grid>
+                        <Grid item xs={3}>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
             </Dialog>
             <Drawer anchor={"right"} open={this.state.openThemes} onClose={this.closeOrOpenThemes.bind(this)}>
                 {
